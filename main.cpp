09516f28cfdf94747f9df27807bfa6935239c81b
jasmine-zero <jasmine.zero.x@gmail.com>
2026-09-24 09:46:57 +0530

    Add main.cpp with Hello World implementation

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define endl "\n"
+#define int long long
+#define mod_ 1000000007
+#define mod__ 998244353
+#define at_index find_by_order
+#define index_of order_of_key
+#define fastIO                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T> using ordered_set = tree<
+    T,
+    null_type,
+    less<T>,
+    rb_tree_tag,
+    tree_order_statistics_node_update
+>;
+
+
+void pixel() {
+    cout << "Hello world!" << endl;
+}
+
+
+int32_t main() {
+    fastIO;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        pixel();
+    }
+    return 0;
+}
+
+
+
+/*
+
+.                      _.--._
+.                 _.-.'      `.-._
+.               .' ./`--...--' \  `.
+.      .-.      `.'.`--.._..--'   .'
+.  _..'.-.`-._.'( (-..__    __..-'
+.   >.'   `-...' ) )    ````
+.   '           / /
+.          .._.'.'
+.           >.-'
+.           '
+
+
+*/
